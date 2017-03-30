844afd7780ebed44eab3cc419a5f18f9bc37a726
Soleil Rojas <soleilrojas@outlook.com>
2017-03-30 16:23:28 -0600

    Trying to get the context out of each file

---
+++ PRS/PRS/Context.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libraries.PRS
+{
+    class Context
+    {
+        internal byte[] src;
+        internal byte[] dst;
+        internal int src_pos;
+        internal int dst_pos;
+        
+        internal byte flags;
+        internal int flag_pos;
+        internal int bit_pos;
+
+        internal Context(byte[] data)
+        {
+            src = data;
+            dst = null;
+            src_pos = 0;
+            dst_pos = 0;
+
+            flags = 0;
+            bit_pos = 0;
+        }
+        internal void Reset()
+        {
+            src_pos = 0;
+            dst_pos = 0;
+
+            flags = 0;
+            bit_pos = 0;
+        }
+
+        internal void SetDst(int length)
+        {
+            dst = new byte[length];
+        }
+        internal byte[] GetDst()
+        {
+            return dst;
+        }
+        internal int GetDstPostion()
+        {
+            return dst_pos;
+        }
+    }
+}
--- PRS/PRS/Decompress.cs
+++ PRS/PRS/Decompress.cs
@@ -12,7 +12,7 @@
         /// <returns></returns>
         public static byte[] Decompress(byte[] data)
         {
-            Decompress_Context ctx = new Decompress_Context(data);
+            Context ctx = new Context(data);
 
             // Get decompressed size, no more no less
             int decompressed_size = Process(ctx, true);
@@ -26,7 +26,7 @@
             return ctx.GetDst();
         }
 
-        private static int Process(Decompress_Context ctx, bool size_only)
+        private static int Process(Context ctx, bool size_only)
         {
             int flag = 0;
             int size = 0;
@@ -38,37 +38,37 @@
             while (true)
             {
                 // Read the flag bit for this pass.
-                flag = ctx.ReadBit();
+                flag = ReadBit(ctx);
 
                 if (flag == 1)
                 {
                     // Flag bit = 1 -> Simple byte copy from src to dst.
-                    ctx.CopyByte(size_only);
+                    CopyByte(ctx, size_only);
                     continue;
                 }
 
                 // The flag starts with a zero, so it isn't just a simple byte copy.
                 // Read the next bit to see what we have left to do.
-                flag = ctx.ReadBit();
+                flag = ReadBit(ctx);
 
                 if (flag == 0)
                 {
                     // Flag bit = 0 -> short copy.
 
                     // Fetch the two bits needed to determine the size.
-                    flag = ctx.ReadBit();
-                    size = ctx.ReadBit();
+                    flag = ReadBit(ctx);
+                    size = ReadBit(ctx);
                     size = (size | (flag << 1)) + 2;
 
                     // Fetch the offset byte.
-                    offset = ctx.ReadByte();
+                    offset = ReadByte(ctx);
                     offset = (int)(offset | 0xFFFFFF00);
                 }
                 else
                 {
                     // Flag bit = 1->Either long copy or end of file.
 
-                    offset = ctx.ReadShort();
+                    offset = ReadShort(ctx);
                     if (offset == 0)
                     {
                         break;
@@ -80,7 +80,7 @@
 
                     if (size == 0)
                     {
-                        size = ctx.ReadByte();
+                        size = ReadByte(ctx);
                         size += 1;
                     }
                     else
@@ -93,102 +93,59 @@
 
                 while (size > 0)
                 {
-                    ctx.CopyByteAt(size_only, offset);
+                    CopyByteAt(ctx, size_only, offset);
                     size -= 1;
                 }
             }
             return ctx.GetDstPostion();
         }
-    }
-    
-    class Decompress_Context
-    {
-        private byte[] src;
-        private byte[] dst;
-        private int src_pos;
-        private int dst_pos;
 
-        private byte flags;
-        private int bit_pos;
-
-        internal Decompress_Context(byte[] data)
-        {
-            src = data;
-            dst = null;
-            src_pos = 0;
-            dst_pos = 0;
-
-            flags = 0;
-            bit_pos = 0;
-        }
-        internal void Reset()
-        {
-            src_pos = 0;
-            dst_pos = 0;
-
-            flags = 0;
-            bit_pos = 0;
-        }
-
-        internal void SetDst(int length)
-        {
-            dst = new byte[length];
-        }
-        internal byte[] GetDst()
-        {
-            return dst;
-        }
-        internal int GetDstPostion()
-        {
-            return dst_pos;
-        }
-
-        internal int ReadBit()
+        internal static int ReadBit(Context ctx)
         {
             int result;
 
-            if (bit_pos == 0)
+            if (ctx.bit_pos == 0)
             {
-                flags = src[src_pos];
-                src_pos += 1;
-                bit_pos = 8;
+                ctx.flags = ctx.src[ctx.src_pos];
+                ctx.src_pos += 1;
+                ctx.bit_pos = 8;
             }
-            result = flags & 1;
-            flags >>= 1;
-            bit_pos -= 1;
+            result = ctx.flags & 1;
+            ctx.flags >>= 1;
+            ctx.bit_pos -= 1;
             return result;
         }
-        internal int ReadByte()
+        internal static int ReadByte(Context ctx)
         {
             int result;
-            result = src[src_pos];
-            src_pos += 1;
+            result = ctx.src[ctx.src_pos];
+            ctx.src_pos += 1;
             return result;
         }
-        internal int ReadShort()
+        internal static int ReadShort(Context ctx)
         {
             int result;
-            result = src[src_pos] + (src[src_pos + 1] << 8);
-            src_pos += 2;
+            result = ctx.src[ctx.src_pos] + (ctx.src[ctx.src_pos + 1] << 8);
+            ctx.src_pos += 2;
             return result;
         }
 
-        internal void CopyByte(bool copy)
+        internal static void CopyByte(Context ctx, bool copy)
         {
             if (copy)
             {
-                dst[dst_pos] = src[src_pos];
+                ctx.dst[ctx.dst_pos] = ctx.src[ctx.src_pos];
             }
-            src_pos += 1;
-            dst_pos += 1;
+            ctx.src_pos += 1;
+            ctx.dst_pos += 1;
         }
-        internal void CopyByteAt(bool copy, int offset)
+        internal static void CopyByteAt(Context ctx, bool copy, int offset)
         {
             if (copy)
             {
-                dst[dst_pos] = dst[dst_pos + offset];
+                ctx.dst[ctx.dst_pos] = ctx.dst[ctx.dst_pos + offset];
             }
-            dst_pos += 1;
+            ctx.dst_pos += 1;
         }
     }
 }
