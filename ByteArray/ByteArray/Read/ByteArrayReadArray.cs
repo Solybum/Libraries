50f929a32e5cccb7871272a8d2484cb2bbf3d1e1
Soleil Rojas <soleilrojas@outlook.com>
2017-04-02 22:20:50 -0600

    Moved ReadArray operations to their own file.

---
+++ ByteArray/ByteArray/Read/ByteArrayReadArray.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libraries.ByteArray
+{
+    public partial class ByteArray
+    {
+        /// <summary>
+        /// Reads data into the array, without advancing the internal position
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="index">Starting position in the array</param>
+        /// <param name="length">Amount of array values to read</param>
+        /// <param name="position">ByteArray index to read from</param>
+        public void Read(sbyte[] array, int index, int length, int position)
+        {
+            length += index;
+            while (index < length)
+            {
+                array[index] = ReadInt8(position);
+                position += 1;
+                index++;
+            }
+        }
+        /// <summary>
+        /// Reads data into the array, without advancing the internal position
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="index">Starting position in the array</param>
+        /// <param name="length">Amount of array values to read</param>
+        /// <param name="position">ByteArray index to read from</param>
+        public void Read(byte[] array, int index, int length, int position)
+        {
+            length += index;
+            while (index < length)
+            {
+                array[index] = ReadUInt8(position);
+                position += 1;
+                index++;
+            }
+        }
+        /// <summary>
+        /// Reads data into the array, without advancing the internal position
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="index">Starting position in the array</param>
+        /// <param name="length">Amount of array values to read</param>
+        /// <param name="position">ByteArray index to read from</param>
+        public void Read(short[] array, int index, int length, int position)
+        {
+            length += index;
+            while (index < length)
+            {
+                array[index] = ReadInt16(position);
+                position += 2;
+                index++;
+            }
+        }
+        /// <summary>
+        /// Reads data into the array, without advancing the internal position
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="index">Starting position in the array</param>
+        /// <param name="length">Amount of array values to read</param>
+        /// <param name="position">ByteArray index to read from</param>
+        public void Read(ushort[] array, int index, int length, int position)
+        {
+            length += index;
+            while (index < length)
+            {
+                array[index] = ReadUInt16(position);
+                position += 2;
+                index++;
+            }
+        }
+        /// <summary>
+        /// Reads data into the array, without advancing the internal position
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="index">Starting position in the array</param>
+        /// <param name="length">Amount of array values to read</param>
+        /// <param name="position">ByteArray index to read from</param>
+        public void Read(int[] array, int index, int length, int position)
+        {
+            length += index;
+            while (index < length)
+            {
+                array[index] = ReadInt32(position);
+                position += 4;
+                index++;
+            }
+        }
+        /// <summary>
+        /// Reads data into the array, without advancing the internal position
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="index">Starting position in the array</param>
+        /// <param name="length">Amount of array values to read</param>
+        /// <param name="position">ByteArray index to read from</param>
+        public void Read(uint[] array, int index, int length, int position)
+        {
+            length += index;
+            while (index < length)
+            {
+                array[index] = ReadUInt32(position);
+                position += 4;
+                index++;
+            }
+        }
+        /// <summary>
+        /// Reads data into the array, without advancing the internal position
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="index">Starting position in the array</param>
+        /// <param name="length">Amount of array values to read</param>
+        /// <param name="position">ByteArray index to read from</param>
+        public void Read(long[] array, int index, int length, int position)
+        {
+            length += index;
+            while (index < length)
+            {
+                array[index] = ReadInt64(position);
+                position += 8;
+                index++;
+            }
+        }
+        /// <summary>
+        /// Reads data into the array, without advancing the internal position
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="index">Starting position in the array</param>
+        /// <param name="length">Amount of array values to read</param>
+        /// <param name="position">ByteArray index to read from</param>
+        public void Read(ulong[] array, int index, int length, int position)
+        {
+            length += index;
+            while (index < length)
+            {
+                array[index] = ReadUInt64(position);
+                position += 8;
+                index++;
+            }
+        }
+        /// <summary>
+        /// Reads data into the array, without advancing the internal position
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="index">Starting position in the array</param>
+        /// <param name="length">Amount of array values to read</param>
+        /// <param name="position">ByteArray index to read from</param>
+        public void Read(float[] array, int index, int length, int position)
+        {
+            length += index;
+            while (index < length)
+            {
+                array[index] = ReadSingle(position);
+                position += 4;
+                index++;
+            }
+        }
+        /// <summary>
+        /// Reads data into the array, without advancing the internal position
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="index">Starting position in the array</param>
+        /// <param name="length">Amount of array values to read</param>
+        /// <param name="position">ByteArray index to read from</param>
+        public void Read(double[] array, int index, int length, int position)
+        {
+            length += index;
+            while (index < length)
+            {
+                array[index] = ReadDouble(position);
+                position += 8;
+                index++;
+            }
+        }
+        
+        /// <summary>
+        /// Reads data into the array, advancing the internal position
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="index">Starting position in the array</param>
+        /// <param name="length">Amount of array values to read</param>
+        public void Read(sbyte[] array, int index, int length)
+        {
+            this.Read(array, index, length, _position);
+            _position += (length * 1);
+        }
+        /// <summary>
+        /// Reads data into the array, without advancing the internal position
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="index">Starting position in the array</param>
+        /// <param name="length">Amount of array values to read</param>
+        public void Read(byte[] array, int index, int length)
+        {
+            this.Read(array, index, length, _position);
+            _position += (length * 1);
+        }
+        /// <summary>
+        /// Reads data into the array, without advancing the internal position
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="index">Starting position in the array</param>
+        /// <param name="length">Amount of array values to read</param>
+        public void Read(short[] array, int index, int length)
+        {
+            this.Read(array, index, length, _position);
+            _position += (length * 2);
+        }
+        /// <summary>
+        /// Reads data into the array, without advancing the internal position
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="index">Starting position in the array</param>
+        /// <param name="length">Amount of array values to read</param>
+        public void Read(ushort[] array, int index, int length)
+        {
+            this.Read(array, index, length, _position);
+            _position += (length * 2);
+        }
+        /// <summary>
+        /// Reads data into the array, without advancing the internal position
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="index">Starting position in the array</param>
+        /// <param name="length">Amount of array values to read</param>
+        public void Read(int[] array, int index, int length)
+        {
+            this.Read(array, index, length, _position);
+            _position += (length * 4);
+        }
+        /// <summary>
+        /// Reads data into the array, without advancing the internal position
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="index">Starting position in the array</param>
+        /// <param name="length">Amount of array values to read</param>
+        public void Read(uint[] array, int index, int length)
+        {
+            this.Read(array, index, length, _position);
+            _position += (length * 4);
+        }
+        /// <summary>
+        /// Reads data into the array, without advancing the internal position
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="index">Starting position in the array</param>
+        /// <param name="length">Amount of array values to read</param>
+        public void Read(long[] array, int index, int length)
+        {
+            this.Read(array, index, length, _position);
+            _position += (length * 8);
+        }
+        /// <summary>
+        /// Reads data into the array, without advancing the internal position
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="index">Starting position in the array</param>
+        /// <param name="length">Amount of array values to read</param>
+        public void Read(ulong[] array, int index, int length)
+        {
+            this.Read(array, index, length, _position);
+            _position += (length * 8);
+        }
+        /// <summary>
+        /// Reads data into the array, without advancing the internal position
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="index">Starting position in the array</param>
+        /// <param name="length">Amount of array values to read</param>
+        public void Read(float[] array, int index, int length)
+        {
+            this.Read(array, index, length, _position);
+            _position += (length * 4);
+        }
+        /// <summary>
+        /// Reads data into the array, without advancing the internal position
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="index">Starting position in the array</param>
+        /// <param name="length">Amount of array values to read</param>
+        public void Read(double[] array, int index, int length)
+        {
+            this.Read(array, index, length, _position);
+            _position += (length * 8);
+        }
+    }
+}
---
+++ ByteArray/ByteArray/Write/ByteArrayWriteArray.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libraries.ByteArray
+{
+    public partial class ByteArray
+    {
+
+    }
+}
