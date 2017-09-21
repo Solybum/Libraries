e36dd0919a4e611f1d76b4d1e843c3b8fe6deed5
Soleil Rojas <soleilrojas@outlook.com>
2017-09-21 04:52:28 -0600

    Removed unused usings

--- MVVM/MVVM/ViewModelBase.cs
+++ MVVM/MVVM/ViewModelBase.cs
@@ -4,7 +4,6 @@ using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Runtime.CompilerServices;
 using MVVM.Messaging;
 
